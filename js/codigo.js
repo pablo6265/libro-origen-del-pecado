eb9989372e2b72556bec8f7260fbcb961d21e5a3
pablo6265 <pablosta_1@hotmail.com>
2024-12-18 20:07:21 -0500

    origen del pecado libro

---
+++ js/codigo.js
@@ -0,0 +1,27 @@
+// function abrir(){
+
+// document.getElementById('modal').style.display = 'block'
+
+// }
+
+// function cerrar(){
+//     document.getElementById('modal').style.display = 'none'
+// }
+const containerM = document.querySelector('.container-modal');
+const containerM1 = document.querySelector('.container-modal-1');
+
+const closeModal = document.querySelector('.close-modal')
+const btnM = document.querySelector('.btn')
+
+function containerModal(){
+    containerM.style.display = "block";
+}
+function closeModal1(){
+    containerM.style.display = "none";
+}
+// function containerModal2(){
+//     containerM1.style.display = "block";
+// }
+// function closeModal2(){
+//     containerM1.style.display = "none";
+// }
